31e6f2d06626d33607f034a06ee08f1aca1372df
Kim Johansson <maxiepax@gmail.com>
2022-12-12 22:48:42 +0100

    added vSphere integration to collect MOREF data

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/maxiepax/vmw-logscrubber
 
 go 1.18
 
-require github.com/otiai10/copy v1.9.0
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/otiai10/copy v1.9.0
+	github.com/vmware/govmomi v0.30.0
+)
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import (
 	"bufio"
 	"bytes"
 	"compress/gzip"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/vmware/govmomi/session/cache"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 func scrub(rd io.Reader, wr io.Writer, si []string) error {
@@ -65,7 +71,6 @@ func scrubFile(fsrc, fdst string, si []string) error {
 
 	//create folder to place file in
 	os.MkdirAll(filepath.Dir(fdst), os.ModePerm)
-	spew.Dump(filepath.Dir(fdst))
 
 	//create the file to place scrubbed data in
 	fw, err := os.OpenFile(fdst, os.O_WRONLY+os.O_CREATE, os.ModePerm)
@@ -197,20 +202,165 @@ func fileType(f io.Reader) (string, []byte, error) {
 	return contentType, buf, nil
 }
 
+// getEnvString returns string from environment variable.
+func getEnvString(v string, def string) string {
+	r := os.Getenv(v)
+	if r == "" {
+		return def
+	}
+
+	return r
+}
+
+// getEnvBool returns boolean from environment variable.
+func getEnvBool(v string, def bool) bool {
+	r := os.Getenv(v)
+	if r == "" {
+		return def
+	}
+
+	switch strings.ToLower(r[0:1]) {
+	case "t", "y", "1":
+		return true
+	}
+
+	return false
+}
+
+const (
+	envURL      = "GOVMOMI_URL"
+	envUserName = "GOVMOMI_USERNAME"
+	envPassword = "GOVMOMI_PASSWORD"
+	envInsecure = "GOVMOMI_INSECURE"
+)
+
+var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
+var urlFlag = flag.String("url", getEnvString(envURL, ""), urlDescription)
+
+var insecureDescription = fmt.Sprintf("Don't verify the server's certificate chain [%s]", envInsecure)
+var insecureFlag = flag.Bool("insecure", getEnvBool(envInsecure, false), insecureDescription)
+
+func processOverride(u *url.URL) {
+	envUsername := os.Getenv(envUserName)
+	envPassword := os.Getenv(envPassword)
+
+	// Override username if provided
+	if envUsername != "" {
+		var password string
+		var ok bool
+
+		if u.User != nil {
+			password, ok = u.User.Password()
+		}
+
+		if ok {
+			u.User = url.UserPassword(envUsername, password)
+		} else {
+			u.User = url.User(envUsername)
+		}
+	}
+
+	// Override password if provided
+	if envPassword != "" {
+		var username string
+
+		if u.User != nil {
+			username = u.User.Username()
+		}
+
+		u.User = url.UserPassword(username, envPassword)
+	}
+}
+
+// NewClient creates a vim25.Client for use in the examples
+func NewClient(ctx context.Context) (*vim25.Client, error) {
+	// Parse URL from string
+	u, err := soap.ParseURL(*urlFlag)
+	if err != nil {
+		return nil, err
+	}
+
+	// Override username and/or password as required
+	processOverride(u)
+
+	// Share govc's session cache
+	s := &cache.Session{
+		URL:      u,
+		Insecure: *insecureFlag,
+	}
+
+	c := new(vim25.Client)
+	err = s.Login(ctx, c, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Run calls f with Client create from the -url flag if provided,
+// otherwise runs the example against vcsim.
+func Run(f func(context.Context, *vim25.Client) error) {
+
+	var err error
+	var c *vim25.Client
+
+	ctx := context.Background()
+	c, err = NewClient(ctx)
+	if err == nil {
+		err = f(ctx, c)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
-	spew.Dump("begin")
-
-	//index that will be used for scrubbing
+	//initiate index that will be used for scrubbing
 	scrubIndex := []string{}
-	scrubIndex = append(scrubIndex, "sfo01-w01-esx05.vmlab.se", "host-05")
-	scrubIndex = append(scrubIndex, "sfo01-w01-esx07.vmlab.se", "host-02")
 
 	//define flags, we want a starting directory, and the directory to output to
-	inpath := flag.String("i", ".", "in")
-	outpath := flag.String("o", "scrubbed", "out")
-	//do the actual parse
+	inpath := flag.String("in", ".", "in")
+	outpath := flag.String("out", "scrubbed", "out")
+	//custom := flag.String("custom", "", "custom")
+
+	//parse for defined flags by user
 	flag.Parse()
+
+	//connect to vCenter to obtain MoReF Objects.
+	Run(func(ctx context.Context, c *vim25.Client) error {
+		// Create a view of Network types
+		m := view.NewManager(c)
+
+		v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, nil, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var content []types.ObjectContent
+
+		//retrieve all MoReFs availble in vCenter.
+		err = v.Retrieve(ctx, nil, []string{"name"}, &content)
+		if err != nil {
+			return err
+		}
+
+		//check length of reponse.
+		l := len(content)
+		fmt.Printf("Retrieved %d entrys from vCenter \n", l)
+
+		//iterate through response and push into scrubIndex
+		for _, item := range content {
+			s := strings.Split(item.Obj.String(), ":")
+			scrubIndex = append(scrubIndex, item.PropSet[0].Val.(string), s[1])
+		}
+
+		return nil
+	})
+
+	//check if custom searchIndex needs to be added
+	//TODO:
 
 	//build a list of files that need to be scrubbed
 	list := buildFileList(*inpath)
@@ -222,4 +372,5 @@ func main() {
 			log.Println(err)
 		}
 	}
+
 }
